01aa6c3d1c26cc2bc3c743ce31e75bceeac26ebc
Shane Wade <shanebramble96@hotmail.com>
2023-05-08 18:50:59 -0500

    display reponse data on page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,8 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
 var getUserRepos = function (user) {
   // format the github api url.
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -5,9 +10,34 @@ var getUserRepos = function (user) {
   // make a request to the url.
   fetch(apiUrl).then(function (response) {
     response.json().then(function (data) {
-      console.log(data);
+      displayRepos(data, user);
     });
   });
 };
 
-getUserRepos();
+var displayRepos = function (repos, searchTerm){
+  // clear old content
+  repoContainerEl.textContent = "";
+  repoSearchTerm.textContent = searchTerm;
+};
+
+
+
+
+
+var formSubmitHandler = function (event){
+  event.preventDefault();
+  // get value from input element. 
+  var username = nameInputEl.value.trim();
+
+  if (username){
+    getUserRepos(username);
+    nameInputEl.value = "";
+  }else {
+    alert("Please enter a Github username");
+  }
+};
+
+
+
+userFormEl.addEventListener("submit", formSubmitHandler);
