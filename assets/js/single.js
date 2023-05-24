ab20a8e6e0b38df74618045e7a812a147541781d
Shane Wade <shanebramble96@hotmail.com>
2023-05-24 18:32:52 -0500

    dynamically created issues for a selected repository.

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,10 +1,29 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+  // grab repo name from url query string.
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  // check for a valid query string.
+  if (repoName) {
+    // display repo name on the page.
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to homepage.
+    document.location.replace("./index.html");
+  }
+};
 
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-
+  
+  // make a get request to url.
   fetch(apiUrl).then(function (response) {
+    // request was sucessful.
     if (response.ok) {
       response.json().then(function (data) {
         displayIssues(data);
@@ -15,7 +34,8 @@ var getRepoIssues = function (repo) {
         }
       });
     } else {
-      alert("There is a problem with your request!");
+      // if not successful, redirect to homepage.
+      document.location.replace("./index.html");
     }
   });
 };
@@ -69,4 +89,5 @@ var displayWarning = function (repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+// getRepoIssues("facebook/react");
+getRepoName();
